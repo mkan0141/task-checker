8210f5549b1fa5acffa51a85717504beec254518
mkan0141 <kanzume0141@gmail.com>
2022-12-22 22:55:51 +0900

    main chore(src-tauri): show and hide window when click on menubar icon

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,6 +3,9 @@
     windows_subsystem = "windows"
 )]
 
+use tauri::Manager;
+use tauri::{SystemTray, SystemTrayEvent, PhysicalPosition};
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
@@ -10,7 +13,29 @@ fn greet(name: &str) -> String {
 }
 
 fn main() {
+    let tray = SystemTray::new();
+
     tauri::Builder::default()
+        .system_tray(tray)
+        .on_system_tray_event(move |app, event| match event {
+            SystemTrayEvent::LeftClick { position, size, .. } => {
+                let window = app.get_window("main").unwrap();
+                let is_visible = window.is_visible().unwrap();
+ 
+                if is_visible {
+                    window.hide().unwrap();
+                } else {
+                    let window_size = window.outer_size().unwrap();
+                    let position = PhysicalPosition {
+                        x: (position.x as i32) + (size.width as i32 / 2) - (window_size.width as i32 / 2),
+                        y: (position.y as i32) - (window_size.height as i32)
+                    };                    
+                    window.set_position(tauri::Position::Physical(position)).unwrap();
+                    window.show().unwrap();
+                }
+            }
+            _ => {}
+        })
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
